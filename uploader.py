234a22eb7143943e6314c5ea9846adb80f3e0b20
Connor <Nemesis0320@users.noreply.github.com>
2026-02-10 09:01:26 -0600

    Update uploader.py

--- uploader.py
+++ uploader.py
@@ -227,9 +227,10 @@ def render_video(audio, output, episode_title=None, season_label=None):
         "[bg][circ_wave]overlay=(W-w)/2:(H-h)/2[bg_wave];"
         f"[bg_wave]drawtext=fontfile={FONT_FILE}:"
         f"text='{safe_podcast_title}\\n{safe_season_label}\\n{safe_episode_title}':"
-        "x=(w-text_w)/2:y=60:fontsize=32:line_spacing=10:fontcolor=white[bg_text];"
-        f"[bg_text]drawtext=fontfile={FONT_FILE}:text='{safe_ticker_text}':"
-        "x=w-mod(t*120\\,w+text_w):y=h-60:fontsize=26:fontcolor=white[final];"
+        "x=(w-text_w)/2:y=60:fontsize=32:line_spacing=10:fontcolor=white: [bg_text];"
+        f"[bg_text]drawtext=fontfile={FONT_FILE}:"
+        f"text='{safe_ticker_text}':"
+        "x=w-mod(t*120\\,w+text_w):y=h-60:fontsize=26:fontcolor=white: [final];"
         "[final]fade=t=in:st=0:d=0.8[final_faded]"
     )
 
