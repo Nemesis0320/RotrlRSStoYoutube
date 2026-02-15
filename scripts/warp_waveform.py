44847f78f664fa389a823183b6d2225c3232dbdd
Connor <Nemesis0320@users.noreply.github.com>
2026-02-14 21:36:31 -0600

    Update warp_waveform.py

--- scripts/warp_waveform.py
+++ scripts/warp_waveform.py
@@ -5,8 +5,10 @@ WIDTH = 1200
 HEIGHT = 1200
 CENTER_X = 600
 CENTER_Y = 555
-R_MIN = 240
-R_MAX = 280
+
+# Thin ring around the emblem
+R_MIN = 260
+R_MAX = 265
 
 wave = Image.open("wave_linear.png").convert("L")
 wave_np = np.array(wave)
@@ -23,7 +25,7 @@ for y in range(HEIGHT):
             continue
         a = (np.arctan2(dy, dx) + np.pi) / (2 * np.pi)
         sx = int(a * (w_src - 1))
-        sy = int(((r - R_MIN) / (R_MAX - R_MIN)) * (h_src - 1))
+        sy = 0
         out[y, x] = wave_np[sy, sx]
 
 Image.fromarray(out).save("wave_circle.png")
