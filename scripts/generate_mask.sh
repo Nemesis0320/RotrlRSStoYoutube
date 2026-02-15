9148b4b8bc0bd38a12cd2dfe8ecd225ddf68a6b3
Connor <Nemesis0320@users.noreply.github.com>
2026-02-14 20:57:27 -0600

    Update generate_mask.sh

--- scripts/generate_mask.sh
+++ scripts/generate_mask.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-echo ">>> RUNNING TRUE MASK GENERATOR v9 <<<"
+echo ">>> RUNNING TRUE MASK GENERATOR v10 <<<"
 
 mkdir -p assets
 
-magick convert -size 1200x1200 xc:none \
+convert -size 1200x1200 xc:none \
   -fill white -draw "ellipse 600,555 260,270 0,360" \
   -fill none -draw "ellipse 600,555 240,250 0,360" \
   -alpha copy assets/circle_mask_1200.png
